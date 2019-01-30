e70957533ab146a19ff5513105f743ae4dbc2df1
Searge <ashikcygan@gmail.com>
2019-01-30 21:12:23 +0200

    Mr. Stick Man → colliders & sprite

--- MrStickman/stickman.py
+++ MrStickman/stickman.py
@@ -37,8 +37,12 @@ class Game:
             time.sleep(0.01)
 
 
-class Coords():
-    """docstring for Coords"""
+class Coords:
+    """ Вказує позиції об'єктів на ігровому екрані.
+        Зберігає верхню ліву (x1, y1)
+        і нижню праву (x2, y2) координати
+        будь-якого компонента гри.
+    """
 
     def __init__(self, x1=0, y1=0, x2=0, y2=0):
         super(Coords, self).__init__()
@@ -49,10 +53,14 @@ class Coords():
 
 
 def within_x(co1, co2):
+    """ Приймає два аргументи (об'єкти класу Coords) та визначає,
+        чи один набір координати x (x1, x2)
+        перетнув другий набір координат x (x1, x2)
+    """
     if (co2.x2 > co1.x1 > co2.x1) \
             or (co2.x2 > co1.x2 > co2.x1) \
             or (co1.x2 > co2.x1 > co1.x1) \
-            or (co1.x1 > co2.x2 > co1.x1):
+            or (co1.x2 > co2.x2 > co1.x1):
         return True
     else:
         return False
@@ -62,10 +70,47 @@ def within_y(co1, co2):
     if (co2.y2 > co1.y1 > co2.y1) \
             or (co2.y2 > co1.y2 > co2.y1) \
             or (co1.y2 > co2.y1 > co1.y1) \
-            or (co1.y1 > co2.y2 > co1.y1):
+            or (co1.y2 > co2.y2 > co1.y1):
         return True
     else:
         return False
+
+
+def collided_left(co1, co2):
+    if within_y(co1, co2):
+        if co2.x2 >= co1.x1 >= co2.x1:
+            return True
+    return False
+
+
+def collided_right(co1, co2):
+    if within_y(co1, co2):
+        if co2.x2 >= co1.x2 >= co2.x1:
+            return True
+    return False
+
+
+def collided_top(co1, co2):
+    if within_x(co1, co2):
+        if co2.y2 >= co1.y1 >= co2.y1:
+            return True
+    return False
+
+
+def collided_bottom(y, co1, co2):
+    if within_x(co1, co2):
+        y_calc = co1.y2 + y
+        if co2.y2 >= y_calc >= co2.y1:
+            return True
+    return False
+
+
+class Sprite:
+    """docstring for Sprite"""
+    def __init__(self, game):
+        super(Sprite, self).__init__()
+        self.game = game
+
 
 
 go = Game()
