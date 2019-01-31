56480417dcabd05e1f41470d478a5b54cf276774
Searge <ashikcygan@gmail.com>
2019-01-31 15:49:37 +0200

    Mr. Stick Man → StickMan(Sprite)

--- MrStickman/stickman.py
+++ MrStickman/stickman.py
@@ -143,6 +143,50 @@ class Platform(Sprite):
         self.coordinates = Coords(x, y, x + width, y + height)
 
 
+class StickMan(Sprite):
+    """docstring for StickMan"""
+
+    def __init__(self, game):
+        Sprite.__init__(self, game)
+        self.images_left = [
+            tk.PhotoImage(file=spr + 'stickman-L1'),
+            tk.PhotoImage(file=spr + 'stickman-L2'),
+            tk.PhotoImage(file=spr + 'stickman-L3')
+        ]
+        self.images_right = [
+            tk.PhotoImage(file=spr + 'stickman-R1'),
+            tk.PhotoImage(file=spr + 'stickman-R2'),
+            tk.PhotoImage(file=spr + 'stickman-R3')
+        ]
+        self.image = game.canvas.create_image(200, 470,
+                                              image=self.images_left[0],
+                                              anchor='nw')
+        self.x = -2
+        self.y = 0
+        self.current_image = 0
+        self.current_image_add = 1
+        self.jump_count = 0
+        self.last_time = time.time()
+        self.coordinates = Coords()
+        game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        game.canvas.bind_all('<KeyPress-Right', self.turn_right)
+        game.canvas.bind_all('<space', self.jump)
+
+        def turn_left(self, evt):
+            if self.y == 0:
+                self.x = -2
+
+        def turn_right(self, evt):
+            if self.y == 0:
+                self.x = 2
+
+        def jump(self, evt):
+            if self.y == 0:
+                self.y = -4
+                self.jump_count = 0
+
+
+
 go = Game()
 
 platform1 = Platform(go, tk.PhotoImage(file=spr + plate[0]),
