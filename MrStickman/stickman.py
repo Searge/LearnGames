58eaf238e5419b3e4b5d0fa56e8190079077a1ed
Searge <ashikcygan@gmail.com>
2019-02-02 20:02:46 +0200

    Mr. Stick Man >> Move 2iter

--- MrStickman/stickman.py
+++ MrStickman/stickman.py
@@ -39,7 +39,7 @@ class Game:
 
     def mainloop(self):
         while True:
-            if self.running:
+            if self.running == True:
                 for sprite in self.sprites:
                     sprite.move()
             self.root.update_idletasks()
@@ -149,14 +149,14 @@ class StickMan(Sprite):
     def __init__(self, game):
         Sprite.__init__(self, game)
         self.images_left = [
-            tk.PhotoImage(file=spr + 'stickman-L1'),
-            tk.PhotoImage(file=spr + 'stickman-L2'),
-            tk.PhotoImage(file=spr + 'stickman-L3')
+            tk.PhotoImage(file=spr + 'stickman-L1.gif'),
+            tk.PhotoImage(file=spr + 'stickman-L2.gif'),
+            tk.PhotoImage(file=spr + 'stickman-L3.gif')
         ]
         self.images_right = [
-            tk.PhotoImage(file=spr + 'stickman-R1'),
-            tk.PhotoImage(file=spr + 'stickman-R2'),
-            tk.PhotoImage(file=spr + 'stickman-R3')
+            tk.PhotoImage(file=spr + 'stickman-R1.gif'),
+            tk.PhotoImage(file=spr + 'stickman-R2.gif'),
+            tk.PhotoImage(file=spr + 'stickman-R3.gif')
         ]
         self.image = game.canvas.create_image(200, 470,
                                               image=self.images_left[0],
@@ -169,90 +169,114 @@ class StickMan(Sprite):
         self.last_time = time.time()
         self.coordinates = Coords()
         game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
-        game.canvas.bind_all('<KeyPress-Right', self.turn_right)
-        game.canvas.bind_all('<space', self.jump)
+        game.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        game.canvas.bind_all('<space>', self.jump)
 
-        def turn_left(self, evt):
-            if self.y == 0:
-                self.x = -2
+    def turn_left(self, evt):
+        if self.y == 0:
+            self.x = -2
 
-        def turn_right(self, evt):
-            if self.y == 0:
-                self.x = 2
+    def turn_right(self, evt):
+        if self.y == 0:
+            self.x = 2
 
-        def jump(self, evt):
-            if self.y == 0:
-                self.y = -4
-                self.jump_count = 0
+    def jump(self, evt):
+        if self.y == 0:
+            self.y = -4
+            self.jump_count = 0
 
-        def animate(self):
-            if self.x != 0 and self.y == 0:
-                if time.time() - self.last_time > 0.1:
-                    self.last_time = time.time()
-                    self.current_image += self.current_image_add
-                    if self.current_image >= 2:
-                        self.current_image_add = -1
-                    if self.current_image <= 0:
-                        self.current_image_add = 1
-            if self.x < 0:
-                if self.y != 0:
-                    self.game.canvas.itemconfig(self.image, image=self.images_left[2])
-                else:
-                    self.game.canvas.itemconfig(self.image, image=self.images_left[self.current_image])
-            elif self.x > 0:
-                if self.y != 0:
-                    self.game.canvas.itemconfig(self.image, image=self.images_right[2])
-                else:
-                    self.game.canvas.itemconfig(self.image, image=self.images_right[self.current_image])
+    def animate(self):
+        if self.x != 0 and self.y == 0:
+            if time.time() - self.last_time > 0.1:
+                self.last_time = time.time()
+                self.current_image += self.current_image_add
+                if self.current_image >= 2:
+                    self.current_image_add = -1
+                if self.current_image <= 0:
+                    self.current_image_add = 1
+        if self.x < 0:
+            if self.y != 0:
+                self.game.canvas.itemconfig(
+                    self.image, image=self.images_left[2])
+            else:
+                self.game.canvas.itemconfig(
+                    self.image,
+                    image=self.images_left[self.current_image])
+        elif self.x > 0:
+            if self.y != 0:
+                self.game.canvas.itemconfig(
+                    self.image, image=self.images_right[2])
+            else:
+                self.game.canvas.itemconfig(
+                    self.image,
+                    image=self.images_right[self.current_image])
 
-        def coords(self):
-            xy = self.game.canvas.coords(self.image)
-            self.coordinates.x1 = xy[0]
-            self.coordinates.y1 = xy[1]
-            self.coordinates.x2 = xy[0] + 27
-            self.coordinates.y2 = xy[0] + 30
-            return self.coordinates
+    def coords(self):
+        xy = self.game.canvas.coords(self.image)
+        self.coordinates.x1 = xy[0]
+        self.coordinates.y1 = xy[1]
+        self.coordinates.x2 = xy[0] + 27
+        self.coordinates.y2 = xy[0] + 30
+        return self.coordinates
 
-        def move(self):
-            self.animate()
-            if self.y < 0:
-                self.jump_count += 1
-                if self.jump_count > 20:
-                    self.jump_count -= 1
-            co = self.coords()
-            left = True
-            right = True
-            top = True
-            bottom = True
-            falling = True
-            if self.y > 0 and co.y2 >= self.game.canvas_height:
-                self.y = 0
-                bottom = False
-            elif self.y < 0 and co.y1 <= 0:
-                self.y = 0
+    def move(self):
+        self.animate()
+        if self.y < 0:
+            self.jump_count += 1
+            if self.jump_count > 20:
+                self.jump_count -= 1
+        co = self.coords()
+        left = True
+        right = True
+        top = True
+        bottom = True
+        falling = True
+        if self.y > 0 and co.y2 >= self.game.canvas_height:
+            self.y = 0
+            bottom = False
+        elif self.y < 0 and co.y1 <= 0:
+            self.y = 0
+            top = False
+
+        if self.x > 0 and co.x2 >= self.game.canvas_height:
+            self.x = 0
+            right = False
+        elif self.x < 0 and co.x1 <= 0:
+            self.x = 0
+            left = False
+
+        for sprite in self.game.sprites:
+            if sprite == self:
+                continue
+            sprite_co = sprite.coords()
+            if top and self.y < 0 and collided_top(co, sprite_co):
+                self.y = -self.y
                 top = False
 
-            if self.x > 0 and co.x2 >= self.game.canvas_height:
-                self.x = 0
-                right = False
-            elif self.x < 0 and co.x1 <= 0:
-                self.x = 0
-                left = False
-
-            for sprite in self.game.sprites:
-                if sprite == self:
-                    continue
-                sprite_co = sprite.coords()
-                if top and self.y < 0 and collided_top(co, sprite_co):
-                    self.y = -self.y
-                    top = False
-
-            if bottom and self.y > 0 and collided_bottom(self.y, co, sprite_co):
+            if bottom and self.y > 0 and collided_bottom(self.y,
+                                                         co, sprite_co):
                 self.y = sprite_co.y1 - co.y2
                 if self.y < 0:
                     self.y = 0
                 bottom = False
                 top = False
+
+            if bottom and falling and self.y == 0 \
+                    and co.y2 < self.game.canvas_height \
+                    and collided_bottom(1, co, sprite_co):
+                falling = False
+
+            if left and self.x < 0 and collided_left(co, sprite_co):
+                self.x = 0
+                left = False
+            if right and self.x > 0 and collided_right(co, sprite_co):
+                self.x = 0
+                left = False
+        if falling and bottom \
+                and self.y == 0 \
+                and co.y2 < self.game.canvas_height:
+            self.y = 4
+        self.game.canvas.move(self.image, self.x, self.y)
 
 
 go = Game()
@@ -278,5 +302,11 @@ platform9 = Platform(go, tk.PhotoImage(file=spr + plate[2]),
 platform10 = Platform(go, tk.PhotoImage(file=spr + plate[2]),
                       230, 200, 32, 10)
 
+go.sprites.extend([platform1, platform2, platform3, platform4, platform5,
+                   platform6, platform7, platform8, platform9, platform10])
+# for i in range(1, 10 + 1):
+#     go.sprites.append('platform' + str(i))
 
+sf = StickMan(go)
+go.sprites.append(sf)
 go.mainloop()
